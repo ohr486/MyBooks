438274270cab5f29f0084e6042e624d7471e1ba0
ohr486 <ohr486@gmail.com>
2018-01-21 17:36:45 +0900

    updage jyanken

--- javascript/learn_react/janken/src/index.js
+++ javascript/learn_react/janken/src/index.js
@@ -1,49 +1,90 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import './index.css'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton'
+import Paper from 'material-ui/Paper';
+import { Tabs, Tab } from 'material-ui/Tabs'
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/table';
+import Jyanken from './Jyanken'
 
 class JyankenGamePage extends Component {
   constructor(props) {
     super(props)
-    this.state = {human: null, computer: null}
+    this.jyanken = new Jyanken()
+    this.state = {scores: [], status: {}, tabIndex: 0}
   }
-  pon(human_hand) {
-    const computer_hand = Math.floor(Math.random() * 3)
-    this.setState({human: human_hand, computer: computer_hand})
+  componentDidMount() {
+    this.getResult()
   }
-  judge() {
-    if (this.state.human == null) {
-      return null
-    } else {
-      return (this.state.computer - this.state.human + 3) % 3
-    }
+  tabChange(ix) {
+    this.setState({tabIndex: ix})
+    this.getResult()
+  }
+  getResult() {
+    this.setState({scores: this.jyanken.getScores()})
+    this.setState({status: this.jyanken.getStatuses()})
+  }
+  pon(te) {
+    this.jyanken.pon(te)
+    this.getResult()
   }
   render() {
     return (
-      <div>
-        <h1>じゃんけんぽん!</h1>
-        <JyankenBox actionPon={(te) => this.pon(te)} />
-        <ScoreBox human={this.state.human} computer={this.state.computer} judgement={this.judge()} />
-      </div>
+      <MuiThemeProvider>
+        <div style={{marginLeft: 30}}>
+          <Header>じゃんけん ポン!</Header>
+          <JyankenBox actionPon={(te) => this.pon(te)} />
+          <Paper style={{width: 400}} zDepth={2}>
+            <Tabs value={this.state.tabIndex} onCChange={(ix) => this.tabChange(ix)}>
+              <Tab label="対戦結果" value={0}>
+                <ScoreList scores={this.state.scores} />
+              </Tab>
+              <Tab label="対戦成績" value={1}>
+                <StatusBox status={this.state.status} />
+              </Tab>
+            </Tabs>
+          </Paper>
+        </div>
+      </MuiThemeProvider>
     )
-  }
-  componentDidMount() {
-    setTimeout(() => {this.pon(1)}, 1000)
-  }
-  shouldComponentUpdate(nnextProps, nextState) {
-    const identical = nextState.human == this.state.human && nextState.computer == this.state.computer
-    if (identical) { console.log("*Identical*") }
-    return !identical
   }
 }
 
+const Header = (props) => (<h1>{props.children}</h1>)
+Header.propTypes = {
+  children: PropTypes.string
+}
+
+const StatusBox = (props) => (
+  <Table>
+    <TableBody displayRowCheckbox={false}>
+      <TableRow displayBorder={false}>
+        <TableHeaderColumn>勝ち</TableHeaderColumn>
+        <TableRowColumn style={judgmentStyle(1)}>{props.status.win}</TableRowColumn>
+      </TableRow>
+      <TableRow displayBorder={false}>
+        <TableHeaderColumn>負け</TableHeaderColumn>
+        <TableRowColumn style={judgmentStyle(2)}>{props.status.lose}</TableRowColumn>
+      </TableRow>
+      <TableRow displayBorder={false}>
+        <TableHeaderColumn>引き分け</TableHeaderColumn>
+        <TableRowColumn style={judgmentStyle(0)}>{props.status.draw}</TableRowColumn>
+      </TableRow>
+    </TableBody>
+  </Table>
+)
+StatusBox.propTypes = {
+  status: PropTypes.object
+}
+
 const JyankenBox = (props) => {
+  const style = {marginLeft: 20}
   return (
-    <div>
-      <button onClick={() => props.actionPon(0)}>グー</button>
-      <button onClick={() => props.actionPon(1)}>チョキ</button>
-      <button onClick={() => props.actionPon(2)}>パー</button>
+    <div style={{marginTop: 40, marginBottom: 30, marginLeft: 30}}>
+      <RaisedButton label="グー" onClick={() => props.actionPon(0)} style={style} />
+      <RaisedButton label="チョキ" onClick={() => props.actionPon(1)} style={style} />
+      <RaisedButton label="パー" onClick={() => props.actionPon(2)} style={style} />
     </div>
   )
 }
@@ -51,24 +92,42 @@ JyankenBox.propTypes = {
   actionPon: PropTypes.func
 }
 
-const ScoreBox = (props) => {
+const ScoreList = (props) => (
+  <Table>
+    <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+      <TableRow>
+        <TableHeaderColumn>時間</TableHeaderColumn>
+        <TableHeaderColumn>人間</TableHeaderColumn>
+        <TableHeaderColumn>コンピューター</TableHeaderColumn>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {props.scores.map((score, ix) => <ScoreListItem key={ix} score={score} />)}
+    </TableBody>
+  </Table>
+)
+ScoreList.propTypes = {
+  scores: PropTypes.array
+}
+
+const ScoreListItem = (props) => {
   const teString = ["グー", "チョキ", "パー"]
-  const judgementString = ["引き分け", "勝ち", "負け"]
+  const judgmentString = ["引き分け", "勝ち", "負け"]
+  const dateHHMMSS = (d) => d.toTimeString().substr(0, 8)
   return (
-    <table>
-      <tbody>
-        <tr><th>あなた</th><td>{teString[props.human]}</td></tr>
-        <tr><th>Computer</th><td>{teString[props.computer]}</td></tr>
-        <tr><th>勝敗</th><td>{judgementString[props.judgement]}</td></tr>
-      </tbody>
-    </table>
+    <TableRow style={judgmentStyle(props.score.judgment)}>
+      <TableRowColumn>{dateHHMMSS(props.score.created_at)}</TableRowColumn>
+      <TableRowColumn>{teString[props.score.human]}</TableRowColumn>
+      <TableRowColumn>{teString[props.score.computer]}</TableRowColumn>
+      <TableRowColumn>{judgmentString[props.score.judgment]}</TableRowColumn>
+    </TableRow>
   )
 }
-ScoreBox.propTypes = {
-  human: PropTypes.number,
-  computer: PropTypes.number,
-  judgement: PropTypes.number
+ScoreListItem.propTypes = {
+  score: PropTypes.object
 }
+
+const judgmentStyle = (judgment) => ({color: ["#000", "#2979FF", "#FF1744"][judgment]})
 
 ReactDOM.render(
   <JyankenGamePage />,
