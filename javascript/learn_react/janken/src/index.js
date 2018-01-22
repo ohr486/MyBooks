9b6eb87c072ff4ecc94b7794475736f7be12fa30
ohr486 <ohr486@gmail.com>
2018-01-22 10:29:40 +0900

    update jyanken

--- javascript/learn_react/janken/src/index.js
+++ javascript/learn_react/janken/src/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import { BrowserRouter, Route, Redirect, Link } from 'react-router-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
 import Paper from 'material-ui/Paper';
-import { Tabs, Tab } from 'material-ui/Tabs'
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/table';
 import Jyanken from './Jyanken'
 
@@ -30,25 +31,27 @@ class JyankenGamePage extends Component {
     this.getResult()
   }
   render() {
+    const tabStyle = {width: 200, height: 50, textAlign: 'center', color: '#fff', backgroudColor: '#01bcd4'}
+    const activeStyle = (path) => Object.assign({borderBottom: `solid 2px ${this.props.location.pathname.match(path) ? '#f00' : '#01bcd4'}`}, tabStyle)
     return (
       <MuiThemeProvider>
         <div style={{marginLeft: 30}}>
           <Header>じゃんけん ポン!</Header>
           <JyankenBox actionPon={(te) => this.pon(te)} />
           <Paper style={{width: 400}} zDepth={2}>
-            <Tabs value={this.state.tabIndex} onCChange={(ix) => this.tabChange(ix)}>
-              <Tab label="対戦結果" value={0}>
-                <ScoreList scores={this.state.scores} />
-              </Tab>
-              <Tab label="対戦成績" value={1}>
-                <StatusBox status={this.state.status} />
-              </Tab>
-            </Tabs>
+            <Link to="/scores"><FlatButton label="対戦結果" style={activeStyle('scores')}/></Link>
+            <Link to="/status"><FlatButton label="対戦成績" style={activeStyle('status')}/></Link>
+            <Route path="/scores" component={() => <ScoreList scores={this.state.scores} />}/>
+            <Route path="/status" component={() => <StatusBox status={this.state.status} />}/>
+            <Route exact path="/" component={() => <Redirect to="/scores" />}/>
           </Paper>
         </div>
       </MuiThemeProvider>
     )
   }
+}
+JyankenGamePage.propTypes = {
+  location: PropTypes.object
 }
 
 const Header = (props) => (<h1>{props.children}</h1>)
@@ -130,6 +133,8 @@ ScoreListItem.propTypes = {
 const judgmentStyle = (judgment) => ({color: ["#000", "#2979FF", "#FF1744"][judgment]})
 
 ReactDOM.render(
-  <JyankenGamePage />,
+  <BrowserRouter>
+    <Route path="/" component={JyankenGamePage} />
+  </BrowserRouter>,
   document.getElementById('root')
 )
