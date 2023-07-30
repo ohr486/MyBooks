2cabf7c36ab6249975171437d79b08de9853097d
ohr486 <ohr486@gmail.com>
2023-07-30 14:34:22 +0900

    add 3.7

--- lang/golang/starting_go/ch03/main.go
+++ lang/golang/starting_go/ch03/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
+var pn = 100
 
 func main() {
 	// 3.5
@@ -24,5 +27,53 @@ func main() {
 	println(1, 2, 3)
 
 	// 3.6
+	pn = pn + 1
+	fmt.Printf("n=%d\n", pn)
 
+	// 3.7
+	n = 256
+	var b byte = byte(n)
+	fmt.Printf("b=%d\n", b)
+
+	n = -1
+	b = byte(n)
+	fmt.Printf("b=%d\n", b)
+
+	fmt.Printf("uint32 max value = %d\n", math.MaxUint32)
+
+	fmt.Printf("value = %v\n", 1.0000000000000000)
+	fmt.Printf("value = %v\n", 1.0000000000000001)
+	fmt.Printf("value = %v\n", 1.0000000000000002)
+	fmt.Printf("value = %v\n", 1.0000000000000003)
+	fmt.Printf("value = %v\n", 1.0000000000000004)
+	fmt.Printf("value = %v\n", 1.0000000000000005)
+	fmt.Printf("value = %v\n", 1.0000000000000006)
+	fmt.Printf("value = %v\n", 1.0000000000000007)
+	fmt.Printf("value = %v\n", 1.0000000000000008)
+	fmt.Printf("value = %v\n", 1.0000000000000009)
+
+	fmt.Printf("value = %v\n", float32(1.0000000000000000))
+	fmt.Printf("value = %v\n", float32(1.0000000000000001))
+	fmt.Printf("value = %v\n", float32(1.0000000000000002))
+	fmt.Printf("value = %v\n", float32(1.0000000000000003))
+	fmt.Printf("value = %v\n", float32(1.0000000000000004))
+	fmt.Printf("value = %v\n", float32(1.0000000000000005))
+	fmt.Printf("value = %v\n", float32(1.0000000000000006))
+	fmt.Printf("value = %v\n", float32(1.0000000000000007))
+	fmt.Printf("value = %v\n", float32(1.0000000000000008))
+	fmt.Printf("value = %v\n", float32(1.0000000000000009))
+
+	r := '松'
+	fmt.Printf("%v", r)
+
+	s := "Goの文字列"
+	fmt.Printf("%v", s)
+
+	s = `
+Goの
+RAW文字列リテラルによる
+複数行に渡る
+文字列
+`
+	fmt.Printf("%v", s)
 }
