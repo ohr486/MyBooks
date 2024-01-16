8eb705f7f2d2ac51e15170c3829199156446aa7d
ohr486 <ohr486@gmail.com>
2024-01-16 23:26:58 +0900

    add ch05 for go

---
+++ lang/golang/learning_go/ch05/ex0516.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func modMap(m map[int]string) {
+	m[2] = "こんにちは"
+	m[3] = "さようなら"
+	delete(m, 1)
+}
+
+func modSlice(s []int) {
+	for k, v := range s {
+		s[k] = v * 2
+	}
+	s = append(s, 10)
+}
+
+func main() {
+	m := map[int]string{
+		1: "1番目",
+		2: "2番目",
+	}
+	modMap(m)
+	fmt.Println(m)
+
+	s := []int{1, 2, 3}
+	modSlice(s)
+	fmt.Println(s)
+}
