affd82f8bb93666cbde75fa36cd8631ec63c9013
ohr486 <ohr486@gmail.com>
2024-01-31 23:48:57 +0900

    add ch15 for go

---
+++ lang/golang/learning_go/ch15/ex1504b.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func Map[T1, T2 any](s []T1, f func(T1) T2) []T2 {
+	r := make([]T2, len(s))
+	for i, v := range s {
+		r[i] = f(v)
+	}
+	return r
+}
+
+func Reduce[T1, T2 any](s []T1, initializer T2, f func(T2, T1) T2) T2 {
+	r := initializer
+	for _, v := range s {
+		r = f(r, v)
+	}
+	return r
+}
+
+func Filter[T any](s []T, f func(T) bool) []T {
+	var r []T
+	for _, v := range s {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return r
+}
+
+func main() {
+	words := []string{"One", "Potato", "Two", "Potato", "Three"}
+	filtered := Filter[string](words, func(s string) bool {
+		return s != "Potato"
+	})
+	lengths := Map[string, int](filtered, func(s string) int {
+		return len(s)
+	})
+	sum := Reduce[int, int](lengths, 0, func(acc int, val int) int {
+		return acc + val
+	})
+
+	var s = []int{10, 20, 30}
+	s = Map(s, func(i int) int {
+		return i * i
+	})
+
+	fmt.Println(filtered)
+	fmt.Println(lengths)
+	fmt.Println(sum)
+	fmt.Println(s)
+}
