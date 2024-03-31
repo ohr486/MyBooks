4bd65eb65f4d800b3b080362b0d643f9c4e08f50
ohr486 <ohr486@gmail.com>
2024-04-01 02:03:30 +0900

    add encode, copy script

---
+++ ai/natual_language_ai/wiki_copy.py
@@ -0,0 +1,51 @@
+import os
+import uuid
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--src_dir", help="source dir", required=True)
+parser.add_argument("--dst_dir", help="destination dir", required=True)
+parser.add_argument("--max_articles", help="max num", default=-1, type=int)
+args = parser.parse_args()
+
+if not os.path.isdir(args.dst_dir):
+    os.mkdir(args.dst_dir)
+    for a in '0123456789abcdef':
+        os.mkdir(args.dst_dir + '/' + a)
+
+n_out = 0
+
+def one_copy(src_file):
+    global n_out
+
+    with open(src_file) as f:
+        ln = f.readlines()
+
+    df = None
+    for text in ln:
+        text = text.strip()
+        # タグでないので記事を保存
+        if not (text.startswith('<doc ') or text.startswith('</doc>')):
+            if len(text) > 0:
+                if df is None:
+                    ui = str(uuid.uuid4())
+                    dst_file = args.dst_dir + '/' + ui[0] + '/' + ui + '.txt'
+                    df = open(dst_file, "w")
+                df.write(text + "\n")
+                df.flush()
+        # タグなので次の記事へ
+        else:
+            if df is not None:
+                df.close()
+                n_out = n_out + 1
+                # 最大数を超えているので終了
+                if args.max_articles > 0 and n_out >= args.max_articles:
+                    exit(0)
+            df = None
+
+    if df is not None:
+        df.close()
+
+for d in os.listdir(args.src_dir):
+    for f in os.listdir(args.src_dir + "/" + d):
+        one_copy(args.src_dir + "/" + d + "/" + f)
