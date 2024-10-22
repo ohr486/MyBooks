cbb5c0cd355087d9cdab946a7063a44ca88c6b76
ohr486 <ohr486@gmail.com>
2024-10-22 22:48:48 +0900

    add rpn

---
+++ lang/rust/jissen_rust/ch02/rpn/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let exp = "6.1 5.2 4.3 * + 3.4 2.5 / 1.6 * -";
+
+    let ans = rpn(exp);
+
+    debug_assert_eq!("26.284", format!("{:.4}", ans));
+
+    println!("{} = {:.4}", exp, ans);
+}
