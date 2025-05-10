a3f3560521dc9751feb899d85e08bc7cc88bbfcd
ohr486 <ohr486@gmail.com>
2025-05-11 01:06:28 +0900

    add webpack-example

---
+++ lang/javascript/react_redux/webpack-example/entry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Hello from './Hello';
+
+//ReactDOM.render(
+//    <Hello />,
+//    document.getElementById('root')
+//);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Hello />);
---
+++ lang/javascript/react_redux/webpack-example/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: 'development',
+
+    entry: './entry.js',
+
+    output: {
+        filename: 'output.js'
+    },
+
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                options: {
+                    presets: [
+                        '@babel/preset-react'
+                    ]
+                }
+            }
+        ]
+    }
+};
