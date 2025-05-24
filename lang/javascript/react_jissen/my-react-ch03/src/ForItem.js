47aaf6a976a88537011fbb3238ed23bfaf101303
ohr486 <ohr486@gmail.com>
2025-05-24 19:49:10 +0900

    add DL icon

--- lang/javascript/react_jissen/my-react-ch03/src/ForItem.js
+++ lang/javascript/react_jissen/my-react-ch03/src/ForItem.js
@@ -1,4 +1,12 @@
+import Download from './Download';
+
 export default function ForItem({ book }) {
+    let dd;
+    if (book.download) {
+        dd = <dd>{book.summary}<Download isbn={book.isbn} /></dd>
+    } else {
+        dd = <dd>{book.summary}</dd>
+    }
     return (
         <>
             <dt>
@@ -6,7 +14,7 @@ export default function ForItem({ book }) {
                     {book.title} ({book.price}円)
                 </a>
             </dt>
-            <dd>{book.summary}</dd>
+            {dd}
         </>
     );
 }
--- lang/javascript/react_jissen/my-react-ch03/src/books.js
+++ lang/javascript/react_jissen/my-react-ch03/src/books.js
@@ -3,21 +3,21 @@ const books = [
         isbn: '978-4-8156-1336-5',
         title: 'これから始めるVue.js 3実践入門',
         price: 3740,
-        summary: 'JavaScriptフレームワーク「Vue.js」について...',
+        summary: 'JavaScriptフレームワーク「Vue.js」について... dl=true',
         download: true
     },
     {
         isbn: '978-4-8156-1336-5-2',
         title: 'これから始めるVue.js 3実践入門-2',
         price: 374000,
-        summary: 'JavaScriptフレームワーク「Vue.js」について...-2',
-        download: true
+        summary: 'JavaScriptフレームワーク「Vue.js」について...-2 dl=false',
+        download: false
     },
     {
         isbn: '978-4-8156-1336-5-3',
         title: 'これから始めるVue.js 3実践入門-3',
         price: 374,
-        summary: 'JavaScriptフレームワーク「Vue.js」について...-3',
+        summary: 'JavaScriptフレームワーク「Vue.js」について...-3 dl=true',
         download: true
     }
 ];
