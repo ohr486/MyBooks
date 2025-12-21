4167efb06bd4619f0da75ef023cf455f3635c722
ohr486 <ohr486@gmail.com>
2025-12-22 00:31:53 +0900

    rust ch3

--- lang/rust/rust_hands_on/ch03/src/main.rs
+++ lang/rust/rust_hands_on/ch03/src/main.rs
@@ -100,143 +100,171 @@ fn main() {
 
     // 構造体を使ってみる
     let taro = Person1 {
-        name:String::from("Taro"),
-        mail:String::from("taro@yamada"),
-        age:39
+        name: String::from("Taro"),
+        mail: String::from("taro@yamada"),
+        age: 39,
     };
     let hanako = Person1 {
-        name:String::from("Hanako"),
-        mail:String::from("hanako@flower"),
-        age:28
+        name: String::from("Hanako"),
+        mail: String::from("hanako@flower"),
+        age: 28,
     };
     print_person1(taro);
     print_person1(hanako);
 
     // 省略記法によるインスタンス作成
-    let taro = person1(
-        String::from("Taro"),
-        String::from("taro@yamada"),
-        39
-    );
-    let hanako = person1(
-        String::from("Hanako"),
-        String::from("hanako@flower"),
-        39
-    );
+    let taro = person1(String::from("Taro"), String::from("taro@yamada"), 39);
+    let hanako = person1(String::from("Hanako"), String::from("hanako@flower"), 39);
     print_person1(taro);
     print_person1(hanako);
 
     // タプル構造体
-    let taro = Person2(
-        String::from("Taro"),
-        String::from("taro@yamada"),
-        39
-    );
-    let hanako = Person2(
-        String::from("Hanako"),
-        String::from("hanako@flower"),
-        28
-    );
+    let taro = Person2(String::from("Taro"), String::from("taro@yamada"), 39);
+    let hanako = Person2(String::from("Hanako"), String::from("hanako@flower"), 28);
     print_person2(taro);
     print_person2(hanako);
 
     // Personに出力メソッドを実装
-    let taro = person3(
-        String::from("Taro"),
-        String::from("taro@yamada"),
-        39
-    );
-    let hanako = person3(
-        String::from("Hanako"),
-        String::from("hanako@flower"),
-        28
-    );
+    let taro = person3(String::from("Taro"), String::from("taro@yamada"), 39);
+    let hanako = person3(String::from("Hanako"), String::from("hanako@flower"), 28);
     taro.print();
     hanako.print();
 
     // &selfのない関数
-    let taro = person4(
-        String::from("Taro"),
-        String::from("taro@yamada"),
-        39
-    );
-    let hanako = person4(
-        String::from("Hanako"),
-        String::from("hanako@flower"),
-        28
-    );
+    let taro = person4(String::from("Taro"), String::from("taro@yamada"), 39);
+    let hanako = person4(String::from("Hanako"), String::from("hanako@flower"), 28);
     taro.print();
     hanako.print();
     println!("Person4's fields: {:?}", Person4::fields());
-    
+
     println!("===== 3-3 トレイト =====");
 
+    // トレイトを利用
+    let taro = person5(String::from("Taro"), String::from("taro@yamada"), 39);
+    let hanako = student5(String::from("Hanako"), String::from("hanako@flower"), 2);
+    taro.print();
+    hanako.print();
 
+    // トレイトを関数で使う
+    //print6(taro);
+    //print6(hanako);
 
+    // 借用を使う
+    print7(&taro);
+    print7(&hanako);
 
+    // 戻り値でのトレイト指定
+    let taro = person8("Taro", "taro@yamada", 39);
+    let hanako = student8("Hanako", "hanako@flower", 28);
+    print8(&taro);
+    print8(&hanako);
 
+    // 構造体の作成関数をBox戻り値にする
+    let taro = person9("Taro", "taro@yamada", 39);
+    let hanako = student9("Hanako", "hanako@flower", 28);
+    print9(&taro);
+    print9(&hanako);
+
+    // 構造体をprintln!で出力させる
+    let taro = person10("Taro", "taro@yamada", 39);
+    let hanako = student10("Hanako", "hanako@flower", 28);
+    println!("{:?}", taro);
+    println!("{:?}", hanako);
+
+    // トレイトのデフォルト実装
+    let taro = person11("Taro", "taro@yamada", 39);
+    let hanako = student11("Hanako", "hanako@flower", 28);
+    taro.print();
+    hanako.print();
+
+    println!("===== 3-4 ジェネリクス =====");
+
+    // ジェネリクスを使ったSample構造体
+
+    let taro = Sample1 {
+        name: String::from("Taro"),
+        value: String::from("this is message."),
+    };
+    let hanako = Sample1 {
+        name: String::from("Hanako"),
+        value: 1234,
+    };
+    println!("{:?}", taro);
+    println!("{:?}", hanako);
+
+    // Sample構造体を作るsample関数を用意する
+    let taro = sample2("Taro", "this is message.");
+    let hanako = sample2("Hanako", 1234);
+    println!("{:?}", taro);
+    println!("{:?}", hanako);
+
+    // SampleにVec<T>フィールドを追加する
+    let taro = sample3("Taro", vec![123, 456, 789]);
+    let hanako = sample3("Hanako", vec!["Hello", "Welcome", "Bye!"]);
+    taro.print_values();
+    hanako.print_values();
 }
 
 // 関数と所有権の移動
-fn print_msg1(msg:String) {
+fn print_msg1(msg: String) {
     println!("Message is {}", msg);
 }
 
 // 戻り値で所有権を返す
-fn print_msg2(msg:String) -> String {
+fn print_msg2(msg: String) -> String {
     println!("Message is {}", msg);
     msg
 }
 
 // 参照
-fn print_msg3(msg:&String) {
+fn print_msg3(msg: &String) {
     println!("Message is \"{}\".", msg);
 }
 
 // シャドーイング
-fn print_msg4(msg:&String) -> String {
+fn print_msg4(msg: &String) -> String {
     let msg = String::from("*** ") + msg + " ***";
     println!("[in func] Message is \"{}\"", msg);
     msg
 }
 
 // 参照した値の変更
-fn print_msg5(msg:&mut String) {
+fn print_msg5(msg: &mut String) {
     msg.push_str("!!!!");
     println!("[in func] Message is \"{}\".", msg);
 }
 
 // 構造体を使ってみる
 struct Person1 {
-    name:String,
-    mail:String,
-    age:i32
+    name: String,
+    mail: String,
+    age: i32,
 }
-fn print_person1(p:Person1) {
+fn print_person1(p: Person1) {
     println!("I'm {}({}). Mail to {}.", p.name, p.age, p.mail);
 }
 
 // 省略記法によるインスタンス作成
-fn person1(name:String, mail:String, age:i32) -> Person1 {
-    Person1 {name, mail, age}
+fn person1(name: String, mail: String, age: i32) -> Person1 {
+    Person1 { name, mail, age }
 }
 
 // タプル構造体
 struct Person2(String, String, i32);
 
-fn print_person2(p:Person2) {
+fn print_person2(p: Person2) {
     println!("I'm {}({}). Mail to {}.", p.0, p.2, p.1);
 }
 
 // Personに出力メソッドを実装
 struct Person3 {
-    name:String,
-    mail:String,
-    age:i32
+    name: String,
+    mail: String,
+    age: i32,
 }
 
-fn person3(name:String, mail:String, age:i32) -> Person3 {
-    Person3 {name, mail, age}
+fn person3(name: String, mail: String, age: i32) -> Person3 {
+    Person3 { name, mail, age }
 }
 
 impl Person3 {
@@ -247,13 +275,13 @@ impl Person3 {
 
 // &selfのない関数
 struct Person4 {
-    name:String,
-    mail:String,
-    age:i32
+    name: String,
+    mail: String,
+    age: i32,
 }
 
-fn person4(name:String, mail:String, age:i32) -> Person4 {
-    Person4 {name, mail, age}
+fn person4(name: String, mail: String, age: i32) -> Person4 {
+    Person4 { name, mail, age }
 }
 
 impl Person4 {
@@ -265,12 +293,210 @@ impl Person4 {
         [
             String::from("name:String"),
             String::from("mail:String"),
-            String::from("age:i32")
+            String::from("age:i32"),
         ]
     }
 }
 
+// トレイトを利用する
+trait Print5 {
+    fn print(&self);
+}
 
+struct Person5 {
+    name: String,
+    mail: String,
+    age: i32,
+}
 
+impl Print5 for Person5 {
+    fn print(&self) {
+        println!("{}<{}>({}).", self.name, self.mail, self.age);
+    }
+}
 
+fn person5(name: String, mail: String, age: i32) -> Person5 {
+    Person5 { name, mail, age }
+}
 
+struct Student5 {
+    name: String,
+    mail: String,
+    grade: i32,
+}
+
+impl Print5 for Student5 {
+    fn print(&self) {
+        println!("grade:{}: {}<{}>.", self.grade, self.name, self.mail);
+    }
+}
+
+fn student5(name: String, mail: String, grade: i32) -> Student5 {
+    Student5 { name, mail, grade }
+}
+
+// トレイトを関数で使う
+//fn print6(ob:impl Print5) {
+//    ob.print();
+//}
+
+// 借用を使う
+fn print7(ob: &impl Print5) {
+    ob.print();
+}
+
+// 戻り値でのトレイト指定
+fn person8(name: &str, mail: &str, age: i32) -> impl Print5 {
+    Person5 {
+        name: String::from(name),
+        mail: String::from(mail),
+        age: age,
+    }
+}
+
+fn student8(name: &str, mail: &str, grade: i32) -> impl Print5 {
+    Student5 {
+        name: String::from(name),
+        mail: String::from(mail),
+        grade,
+    }
+}
+
+fn print8(ob: &impl Print5) {
+    ob.print();
+}
+
+// 構造体の作成関数をBox戻り値にする
+fn person9(name: &str, mail: &str, age: i32) -> Box<dyn Print5> {
+    Box::new(Person5 {
+        name: String::from(name),
+        mail: String::from(mail),
+        age: age,
+    })
+}
+
+fn student9(name: &str, mail: &str, grade: i32) -> Box<dyn Print5> {
+    Box::new(Student5 {
+        name: String::from(name),
+        mail: String::from(mail),
+        grade,
+    })
+}
+
+fn print9(ob: &Box<dyn Print5>) {
+    ob.print();
+}
+
+// 構造体をprintln!で出力させる
+#[derive(Debug)]
+struct Person10 {
+    name: String,
+    mail: String,
+    age: i32,
+}
+
+#[derive(Debug)]
+struct Student10 {
+    name: String,
+    mail: String,
+    grade: i32,
+}
+
+fn person10(name: &str, mail: &str, age: i32) -> Person10 {
+    Person10 {
+        name: String::from(name),
+        mail: String::from(mail),
+        age: age,
+    }
+}
+
+fn student10(name: &str, mail: &str, grade: i32) -> Student10 {
+    Student10 {
+        name: String::from(name),
+        mail: String::from(mail),
+        grade: grade,
+    }
+}
+
+// トレイトのデフォルト実装
+trait Print11 {
+    fn print(&self) {
+        println!("PRINT is not yet implemented...");
+    }
+}
+
+#[derive(Debug)]
+struct Person11 {
+    name: String,
+    mail: String,
+    age: i32,
+}
+
+#[derive(Debug)]
+struct Student11 {
+    name: String,
+    mail: String,
+    grade: i32,
+}
+
+impl Print11 for Person11 {
+    fn print(&self) {
+        println!("{}<{}>({}).", self.name, self.mail, self.age);
+    }
+}
+
+impl Print11 for Student11 {}
+
+fn person11(name: &str, mail: &str, age: i32) -> Person11 {
+    Person11 {
+        name: String::from(name),
+        mail: String::from(mail),
+        age: age,
+    }
+}
+
+fn student11(name: &str, mail: &str, grade: i32) -> Student11 {
+    Student11 {
+        name: String::from(name),
+        mail: String::from(mail),
+        grade: grade,
+    }
+}
+
+// ジェネリクスを使ったSample構造体
+#[derive(Debug)]
+struct Sample1<T> {
+    name: String,
+    value: T,
+}
+
+// Sample構造体を作るsample関数を用意する
+fn sample2<T>(name: &str, value: T) -> Sample1<T> {
+    Sample1 {
+        name: String::from(name),
+        value: value,
+    }
+}
+
+// SampleにVec<T>フィールドを追加する
+#[derive(Debug)]
+struct Sample3<T: core::fmt::Debug> {
+    name: String,
+    values: Vec<T>,
+}
+
+impl<T: core::fmt::Debug> Sample3<T> {
+    fn print_values(&self) {
+        println!("*** {} ***", &self.name);
+        for item in &self.values {
+            println!("{:?}", item);
+        }
+    }
+}
+
+fn sample3<T: core::fmt::Debug>(name: &str, values: Vec<T>) -> Sample3<T> {
+    Sample3 {
+        name: String::from(name),
+        values: values,
+    }
+}
